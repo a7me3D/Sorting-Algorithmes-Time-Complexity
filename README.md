2cf7077ba2cc0be28bca4f8fc52aae0be59533df
ahmed jbeli <40963911+a7me3D@users.noreply.github.com>
2022-12-07 21:38:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,185 @@
+```python
+from time import time   
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from sortFunc import mergeSort,bubbleSort,selectionSort,insertionSort,quickSort,radixSort
+
+import sys
+sys.setrecursionlimit(5000)
+```
+
+
+```python
+#wrapper to count function execution time
+from functools import wraps
+import time
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args):
+        start_time = time.perf_counter()
+        result = func(*args)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        return total_time
+    return timeit_wrapper
+```
+
+
+```python
+#initialize the sorting functions, data array and the array sizes
+sortFuncs = [mergeSort, bubbleSort, selectionSort, insertionSort, quickSort]
+funcExecTimeData = {"mergeSort":[], "bubbleSort":[], "selectionSort":[], "insertionSort":[], "quickSort":[], "radixSort":[]}
+array_sizes = [10, 100, 500, 1000, 2000, 2500,3000,4000]
+```
+
+
+```python
+#loop, execute and store time result
+for size in array_sizes:
+    random_array = np.random.randint(1,999,(1,size))[0]
+    funcExecTimeData["mergeSort"].append(timeit(mergeSort)(random_array,0,size-1))
+    funcExecTimeData["bubbleSort"].append(timeit(bubbleSort)(random_array))
+    funcExecTimeData["selectionSort"].append(timeit(selectionSort)(random_array))
+    funcExecTimeData["insertionSort"].append(timeit(insertionSort)(random_array))
+    funcExecTimeData["quickSort"].append(timeit(quickSort)(random_array,0, size-1))
+    funcExecTimeData["radixSort"].append(timeit(radixSort)(random_array))
+
+```
+
+
+```python
+funcExtimedf = pd.DataFrame(funcExecTimeData, index=array_sizes)
+funcExtimedf
+```
+
+
+
+
+<div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>mergeSort</th>
+      <th>bubbleSort</th>
+      <th>selectionSort</th>
+      <th>insertionSort</th>
+      <th>quickSort</th>
+      <th>radixSort</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>10</th>
+      <td>0.000033</td>
+      <td>0.000013</td>
+      <td>0.000016</td>
+      <td>0.000004</td>
+      <td>0.000036</td>
+      <td>0.000046</td>
+    </tr>
+    <tr>
+      <th>100</th>
+      <td>0.000349</td>
+      <td>0.001062</td>
+      <td>0.001018</td>
+      <td>0.000033</td>
+      <td>0.002253</td>
+      <td>0.000653</td>
+    </tr>
+    <tr>
+      <th>500</th>
+      <td>0.002193</td>
+      <td>0.024256</td>
+      <td>0.023521</td>
+      <td>0.000151</td>
+      <td>0.049557</td>
+      <td>0.001255</td>
+    </tr>
+    <tr>
+      <th>1000</th>
+      <td>0.004158</td>
+      <td>0.090960</td>
+      <td>0.089633</td>
+      <td>0.000306</td>
+      <td>0.197953</td>
+      <td>0.002512</td>
+    </tr>
+    <tr>
+      <th>2000</th>
+      <td>0.009034</td>
+      <td>0.370422</td>
+      <td>0.358095</td>
+      <td>0.000627</td>
+      <td>0.800171</td>
+      <td>0.005073</td>
+    </tr>
+    <tr>
+      <th>2500</th>
+      <td>0.011644</td>
+      <td>0.572640</td>
+      <td>0.559005</td>
+      <td>0.000780</td>
+      <td>1.252967</td>
+      <td>0.006367</td>
+    </tr>
+    <tr>
+      <th>3000</th>
+      <td>0.014188</td>
+      <td>0.825370</td>
+      <td>0.804125</td>
+      <td>0.000937</td>
+      <td>1.805299</td>
+      <td>0.007527</td>
+    </tr>
+    <tr>
+      <th>4000</th>
+      <td>0.019523</td>
+      <td>1.473479</td>
+      <td>1.429348</td>
+      <td>0.001253</td>
+      <td>3.237911</td>
+      <td>0.010086</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+
+
+
+```python
+plt.figure(figsize=(15,10))
+frame = sns.lineplot(data=funcExtimedf, dashes=False,)
+frame.set_ylim(0,1)
+```
+
+
+
+
+    (0.0, 1.0)
+
+
+
+
+    
+![png](bechmarkChart.png)
+    
+
---
+++ sortFunc.py
@@ -0,0 +1,187 @@
+# ------------- MERGE SORT -------------------
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    # create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+ 
+    # Merge the temp arrays back into arr[l..r]
+    i = 0     # Initial index of first subarray
+    j = 0     # Initial index of second subarray
+    k = l     # Initial index of merged subarray
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+ 
+ 
+def mergeSort(arr, l, r):
+    if l < r:
+ 
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l+(r-l)//2
+ 
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+
+# ------------- BUBBLE SORT -------------------
+
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+
+# ------------- INSERTION SORT -------------------
+
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+        
+# ------------- SELECTION SORT -------------------
+
+def selectionSort(array):
+    # Traverse through all array elements
+    for i in range(len(array)):
+
+        # Find the minimum element in remaining unsorted array
+        min_idx = i
+        for j in range(i+1, len(array)):
+            if array[min_idx] > array[j]:
+                min_idx = j
+
+        # Swap the found minimum element with 
+        # the first element        
+        array[i], array[min_idx] = array[min_idx], array[i]
+
+
+
+# ------------- QUICK SORT -------------------
+
+
+# take last element as pivot
+# place the pivot at its correct position in sorted array
+# places all smaller (smaller than pivot) to left of pivot 
+# place  all greater elements to right of pivot
+def partition(arr,low,high):
+    i = ( low-1 )         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low , high):
+        if   arr[j] <= pivot:
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+ 
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return ( i+1 )
+ 
+
+# low  --> Starting index,
+# high  --> Ending index 
+def quickSort(arr,low,high):
+    if low < high:
+        # pi is partitioning index, arr[p] is now at right place
+        pi = partition(arr,low,high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+
+#-------------------- RADIX Sort -------------------
+
+def countingSort(arr, exp1): 
+    n = len(arr)
+ 
+    # The output array elements that will have sorted arr
+    output = [0] * (n)
+ 
+    # initialize count array as 0
+    count = [0] * (10)
+ 
+    # Store count of occurrences in count[]
+    for i in range(0, n):
+        index = arr[i] // exp1
+        count[index % 10] += 1
+ 
+    # Change count[i] so that count[i] now contains actual
+    # position of this digit in output array
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+ 
+    # Build the output array
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp1
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+ 
+    # Copying the output array to arr[],
+    # so that arr now contains sorted numbers
+    i = 0
+    for i in range(0, len(arr)):
+        arr[i] = output[i]
+ 
+# Method to do Radix Sort
+def radixSort(arr):
+    # Find the maximum number to know number of digits
+    max1 = max(arr)
+
+    # Do counting sort for every digit. Note that instead
+    # of passing digit number, exp is passed. exp is 10^i
+    # where i is current digit number
+    exp = 1
+    while max1 / exp >= 1:
+        countingSort(arr, exp)
+        exp *= 10
+ 
